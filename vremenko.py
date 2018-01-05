05c1687dd15f1e19af9c0d13d7059cf3985f051a
dzimislaff <nejc.mobilc@gmail.com>
2018-01-05 20:06:01 +0100

    prva različica

---
+++ vremenko.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# -*- coding: 'UTF-8' -*-
+
+import requests
+import argparse
+from lxml import etree
+# from datetime import datetime
+
+
+BASE = ('http://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/'
+        'observationAms_')
+
+# slovar s kombinacijo povezav: osnova + končnica
+KRAJI = {
+    'Ljubljana': (BASE + 'LJUBL-ANA_BEZIGRAD_latest.xml'),
+    'Novo mesto': (BASE + 'NOVO-MES_latest.xml'),
+    'Rogaška Slatina': (BASE + 'ROGAS-SLA_latest.xml'),
+    'Metlika': (BASE + 'METLIKA_latest.xml'),
+    'Celje': (BASE + 'CELJE_MEDLOG_latest.xml'),
+    'Kredarica': (BASE + 'KREDA-ICA_latest.xml'),
+    'Kočevje': (BASE + 'KOCEVJE_latest.xml'),
+    'Koper': (BASE + 'KOPER_KAPET-IJA_latest.xml'),
+    'Maribor': (BASE + 'MARIBOR_VRBAN-PLA_latest.xml'),
+    'Podčetrtek': (BASE + 'PODCE-TEK_ATOMS-TOP_latest.xml'),
+    'Bilje pri Novi Gorici': (BASE + 'NOVA-GOR_BILJE_latest.xml')
+}
+
+# seznam s potjo do podatka (xpath) v XML-datoteki in imenom kategorije
+# prenos v ločeno datoteko?
+KATEGORIJE = [
+    ['/data/metData/nn_icon-wwsyn_icon', 'Opis vremena'],
+    ['/data/metData/t', 'Temperatura', '°C'],
+    ['/data/metData/rhavg', 'Relativna vlažnost', '%'],
+    ['/data/metData/mslavg', 'Tlak', 'hPa'],
+    ['/data/metData/ddavg_longText', 'Smer vetra'],
+    ['/data/metData/ffavg_val', 'Hitrost vetra', 'm/s'],
+    ['/data/metData/sunset', 'Sončni zahod']
+]
+
+# prevod oznak za vremenske razmere
+# prenos v ločeno datoteko?
+OPIS = {
+    'clear': 'jasno',
+    'mostClear': 'pretežno jasno',
+    'partCloudy': 'delno oblačno',
+    'modCloudy': 'zmerno oblačno',
+    'prevCloudy': 'pretežno oblačno',
+    'overcast': 'oblačno',
+    'FG': 'megla',
+    'RA': 'rosenje',
+    'RASN': 'dež s snegom',
+    'SN': 'sneženje',
+    'TS': 'nevihte',
+    'TSGR': 'nevihta s točo',
+    'lightRA': 'manjše padavine',
+    'modRA': 'zmerne padavine',
+    'overcast_lightRA': 'oblačno z manjšimi padavinami',
+    'overcast_modRA': 'oblačno z zmernimi padavinami',
+    'overcast_heavyRA': 'oblačno z močnimi padavinami',
+    'prevCloudy_lightRA': 'pretežno oblačno z manjšimi padavinami',
+    'overcast_lightSN': 'oblačno z manjšim sneženjem'
+}
+
+
+def pridobi_spletno_stran(naslov=KRAJI["Ljubljana"], kraj="Ljubljana"):
+    r = requests.get(naslov)
+    '''
+    [38:] zaradi konflikta med deklaracijo kodiranja in znaki
+
+    ValueError: Unicode strings with encoding declaration are not supported.\
+    Please use bytes input or XML fragments without declaration.
+    '''
+    stran = etree.fromstring(r.text[38:])
+
+    # zdaj = datetime.now()
+    # # TODO absolute path
+    # datoteka = "data/vremenko_%(k)s_%(d)s.xml"\
+    #             % { "k": kraj, "d": zdaj.strftime('%d-%m-%Y_%H-%M')}
+    # with open(datoteka, "w") as f:
+    #     f.write(r.text[38:])
+
+    return stran
+
+
+def izpis(root, kraj, opis=OPIS):
+    print('Podatki za mesto ' + kraj + '.')
+    for i in KATEGORIJE:
+        try:
+            p = root.xpath(i[0])[0].text
+            # opis vremena
+            if i == KATEGORIJE[0]:
+                print('\t' + i[1] + ': ' + opis[p] + '.')
+            # z enoto
+            elif len(i) == 3:
+                print('\t' + i[1] + ': ' + p + ' ' + i[2] + '.')
+            # brez enote
+            else:
+                print('\t' + i[1] + ': ' + p + '.')
+        except TypeError:
+            print('\t' + i[1] + ': Ni podatka. (TypeError)')
+        except KeyError:
+            print('\t' + i[1] + ': Ni podatka. (KeyError)')
+        except IndexError:
+            print('Podatkov za ta kraj trenutno žal ni.')
+            break
+
+
+def izbira_kraja(KRAJI):
+    '''
+    Uporabnik izbere kraj.
+    Funkcija vrne seznam:
+    - s polno povezavo (osnova + končnica),
+    - z imenom kraja (npr. 'Metlika').
+    '''
+    moznosti = list(KRAJI.keys())
+    while True:
+        print('\nPodatki za kraje, ki so na voljo:')
+        for i in range(len(moznosti)):
+            print('\t' + '(' + str(i + 1) + ')  ' + moznosti[i])
+        try:
+            vnos = int(input('Vnesi izbiro: (1-' + str(len(moznosti)) + ')\n'))
+            if vnos < 1:
+                continue
+        except (ValueError, IndexError):
+            print('Neveljavna izbira.')
+            continue
+        except KeyboardInterrupt:
+            print('\nIzhod.')
+            exit(0)
+        else:
+            break
+    return [KRAJI[(moznosti[vnos - 1])], moznosti[vnos - 1]]
+
+
+def argumenti():
+    parser = argparse.ArgumentParser(
+        description="Preprost program, ki izpiše trenutne vremenske razmere.\
+                    Podatke pridobi z ARSO-ve spletne strani.",
+        # epilog="Primer rabe:"
+    )
+    parser.add_argument("-i", "--izbira", action="store_true",
+                        help="prikaže izbire", default=False)
+    parser.add_argument("-lj", "--ljubljana", action="store_true",
+                        help="prikaže podatke za Ljubljano", default=False)
+    parser.add_argument("-nm", "--novomesto", action="store_true",
+                        help="podatki za Novo mesto", default=False)
+    parser.add_argument("-rs", "--rogaska", action="store_true",
+                        help="podatki za Rogaško Slatino", default=False)
+    return parser.parse_args()
+
+
+def main():
+    args = argumenti()
+    if args.izbira:
+        naslov = izbira_kraja(KRAJI)
+        kraj = naslov[1]
+        root = pridobi_spletno_stran(naslov[0])
+        # print('\n--- Izbrali ste podatke za ' + naslov[1] + '. ---')
+    elif args.novomesto:
+        kraj = "Novo mesto"
+        root = pridobi_spletno_stran(KRAJI[kraj])
+    elif args.rogaska:
+        kraj = 'Rogaška Slatina'
+        root = pridobi_spletno_stran(KRAJI[kraj])
+    else:
+        kraj = 'Ljubljana'
+        root = pridobi_spletno_stran(KRAJI['Ljubljana'])
+    izpis(root, kraj)
+
+
+if __name__ == '__main__':
+    main()
