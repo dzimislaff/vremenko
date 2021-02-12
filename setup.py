ff9d7a0cbd9b69e066eea5b387c65460c3fcf0c3
nejc <nejc.mobilc@gmail.com>
2021-02-12 16:16:23 +0100

    dodan find_packages; pravopisni popravki

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@
 # https://github.com/navdeep-G/setup.py
 # https://github.com/navdeep-G/setup.py/blob/master/setup.py
 # https://click.palletsprojects.com/en/7.x/setuptools/
+# https://godatadriven.com/blog/a-practical-guide-to-using-setup-py/
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import __version__
 
 različica_programa = __version__.__version__
-ime_programa = 'vremenko'
-opis_programa = "Preprost program, ki trenutne vremenske razmere izpiše ali \
-                 shrani v podatkovno bazo. Podatke pridobi z ARSO-ve spletne \
-                 strani."
-spletna_povezava = 'https://github.com/dzimislaff/vremenko'
-email = 'nejc.mobilc@gmail.com'
-avtor = 'nejc'
+ime_programa = "vremenko"
+opis_programa = ("Preprost program, ki trenutne vremenske razmere izpiše ali "
+                 "shrani v podatkovno bazo. Podatke pridobi z ARSO-ve spletne "
+                 "strani.")
+spletna_povezava = "https://github.com/dzimislaff/vremenko"
+email = "nejc.mobilc@gmail.com"
+avtor = "nejc"
 različica_pythona = "!=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, >=3.6.0"
 zahtevani_moduli = [
     "lxml", "python-dateutil", "requests"
@@ -32,19 +33,22 @@ setup(
     author=avtor,
     author_email=email,
     python_requires=različica_pythona,
+    packages=find_packages(include=['vremenko', 'vremenko.*']),
     install_requires=zahtevani_moduli,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     entry_points="""
         [console_scripts]
-        vreme=vremenko.ukaznavrstica:ukaznavrstica
+        vremenko=vremenko.ukaznavrstica:ukaznavrstica
     """,
     classifiers=[
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Natural Language :: Slovenian,'
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Natural Language :: Slovenian,"
     ],
 )
